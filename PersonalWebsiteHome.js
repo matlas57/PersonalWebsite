c77ff51ae6bac6fe3a98bd43f00cb91e7a2a1507
Matan Atlas <matanatlas2002@gmail.com>
2023-09-15 17:58:11 -0400

    responsive projects section

--- PersonalWebsiteHome.js
+++ PersonalWebsiteHome.js
@@ -13,20 +13,20 @@ $(window).resize(function() {
         $(".section-about-me").css("height", "100vh");
     }
 });
-$(window).resize(function() {
-    if ($(window).width() < 1800) { 
-        $(".section-projects").css("height", "300vh");
-    } else {
-        $(".section-projects").css("height", "185vh");
-    }
-});
-$(window).resize(function() {
-    if ($(window).width() < 1100) { 
-        $("#splendor-cover").css("display", "none");
-        $("#splendor-text").css("flex-basis", "100%");
+// $(window).resize(function() {
+//     if ($(window).width() < 1800) { 
+//         $(".section-projects").css("height", "300vh");
+//     } else {
+//         $(".section-projects").css("height", "185vh");
+//     }
+// });
+// $(window).resize(function() {
+//     if ($(window).width() < 1300) { 
+//         $(".project-grid-item-cover").css("display", "none");
+//         $(".project-grid-item-text").css("flex-basis", "100%");
 
-    } else {
-        $("#splendor-cover").css("display", "inline");
-        $("#splendor-text").css("flex-basis", "50%");
-    }
-});
+//     } else {
+//         $(".project-grid-item-cover").css("display", "inline");
+//         $(".project-grid-item-text").css("flex-basis", "50%");
+//     }
+// });
