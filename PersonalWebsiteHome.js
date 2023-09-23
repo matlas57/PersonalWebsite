c6970e451c24c928bb473681aadb7675b1e618b2
Matan Atlas <matanatlas2002@gmail.com>
2023-09-23 16:20:24 -0400

    Version 1

--- PersonalWebsiteHome.js
+++ PersonalWebsiteHome.js
@@ -1,25 +1,4 @@
-
-// $(window).resize(function() {
-//     if ($(window).width() < 992) { 
-//         $(".section-about-me").css("height", "140vh");
-//     } else {
-//         $(".section-about-me").css("height", "100vh");
-//     }
-// });
-// $(window).resize(function() {
-//     if ($(window).width() < 1800) { 
-//         $(".section-projects").css("height", "300vh");
-//     } else {
-//         $(".section-projects").css("height", "185vh");
-//     }
-// });
-// $(window).resize(function() {
-//     if ($(window).width() < 1300) { 
-//         $(".project-grid-item-cover").css("display", "none");
-//         $(".project-grid-item-text").css("flex-basis", "100%");
-
-//     } else {
-//         $(".project-grid-item-cover").css("display", "inline");
-//         $(".project-grid-item-text").css("flex-basis", "50%");
-//     }
-// });
+if (window.width < 768){
+    document.getElementById("body").innerHTML = 
+    <h3>Sorry, this website is not optimized for mobile devices. Please view on a desktop or laptop.</h3>;
+}
